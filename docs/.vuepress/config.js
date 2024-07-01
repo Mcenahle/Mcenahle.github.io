87d136ad3c092cb303d9882d66c5b36e8615f058
Mcenahle <Mcenahle@outlook.com>
2024-07-01 12:27:56 +0800

    ‘first’

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
+module.exports = {
+  // 路径名为 "/<REPO>/"
+  base: '/mcenahle.github.io/',
+  //...
+}
